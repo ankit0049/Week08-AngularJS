445b23f25bfc339656d8c98d44ff6c5ef250d841
Ankit Rajput <ankitrajputji00@gmail.com>
2025-03-05 22:14:59 +0530

    [Ankit Rajput] Add . Displayed Image Using Dynamic Winding

--- AngularJS/src/app/app.component.ts
+++ AngularJS/src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  message: string = "Ankit Rajput, Hello from BridgeLabz"; 
+export class AppComponent { 
+  title: string = " Ankit AngularJS"; 
+  ImageURL = "https://github.com/user-attachments/assets/eccf9b76-5291-493b-a9be-b0ef482bbe92";
+  ngOnInit():void {
+   this.title = "Ankit Rajput, Hello from BridgeLabz";  
+  }
 }
